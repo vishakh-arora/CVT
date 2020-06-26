08adad34833ee61d2253527d517dd0b743b25dca
vishakh-arora <vishakh.arora29@gmail.com>
2020-06-26 23:29:04 +0000

    Changed config to production

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-#DATA_DIR='/home/centos/data/covid19/'
-#INSTALL_DIR='/home/centos/bin/CVT/'
-#log_file='/tmp/data.log'
-DATA_DIR='/home/varora/data/'
-INSTALL_DIR='/home/varora/CVT/'
-log_file="/dev/tty"
+DATA_DIR='/home/centos/data/covid19/'
+INSTALL_DIR='/home/centos/bin/CVT/'
+log_file='/tmp/data.log'
+#DATA_DIR='/home/varora/data/'
+#INSTALL_DIR='/home/varora/CVT/'
+#log_file="/dev/tty"
